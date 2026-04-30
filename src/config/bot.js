7a3b0dc9a5a965968cddf66f1c5b539c1fa2bc6c
GhostKiller297 <luisleisinger297@gmail.com>
2026-04-30 19:48:50 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "You",
+        name: "Going dark",
         // Activity type number (0 = Playing).
         type: 3, 
       },
